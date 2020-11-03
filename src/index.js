c22568d4d32abe1204825b3d399e8844be3ee2ac
Valentin <primarh111@mail.ru>
2020-11-03 11:58:44 +0300

    pagination + popular tags

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "pages/routes";
+import Routes from "routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import TopBar from "components/topBar/TopBar";
 import CurentUserChecker from "components/curentUserChecker/CurentUserChecker";
